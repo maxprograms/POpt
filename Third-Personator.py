de264c208adacca345d5c076ede59f08eb23c3c7
Maxprograms <53878520+Maxprograms@users.noreply.github.com>
2019-08-07 16:09:52 -0400

    Add files via upload

---
+++ Third-Personator.py
@@ -0,0 +1,4 @@
+trdp = input("Enter the description:\n")
+print(trdp.replace(" Our", " Their").replace(" We", " They")
+      .replace(" our", " their").replace(" we", " they" ).replace(" us", " them"))
+
